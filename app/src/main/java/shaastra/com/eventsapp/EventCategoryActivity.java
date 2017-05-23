5a185ccab62b988eb0ece51fdf1903b4b6fccf32
rajatvd <rajatvd@gmail.com>
2017-05-23 13:11:48 +0530

    Event category activity now lists all the events

--- app/src/main/java/shaastra/com/eventsapp/EventCategoryActivity.java
+++ app/src/main/java/shaastra/com/eventsapp/EventCategoryActivity.java
@@ -2,8 +2,11 @@ package shaastra.com.eventsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class EventCategoryActivity extends AppCompatActivity {
 
@@ -18,6 +21,17 @@ public class EventCategoryActivity extends AppCompatActivity {
         ListView eventsView = (ListView) findViewById(R.id.evCagList);
 
         eventCategory = (EventCategory) getIntent().getSerializableExtra("eventCategory");
+
+
+
+        ArrayList<Event> eventsList = eventCategory.events;
+        String[] eventNames = new String[eventsList.size()];
+        for(int i=0;i<eventsList.size();i++){
+            eventNames[i] = eventsList.get(i).eventName;
+        }
+
+        ArrayAdapter<String> eventsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eventNames);
+        eventsView.setAdapter(eventsAdapter);
 
         title.setText(eventCategory.name);
 
