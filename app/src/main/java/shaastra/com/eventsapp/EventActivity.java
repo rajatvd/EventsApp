3cd9547454d0ba02869979e7f28dd6f3fb412561
rajatvd <rajatvd@gmail.com>
2017-05-23 13:48:30 +0530

    Event activity displays all details except phone number

--- app/src/main/java/shaastra/com/eventsapp/EventActivity.java
+++ app/src/main/java/shaastra/com/eventsapp/EventActivity.java
@@ -19,8 +19,23 @@ public class EventActivity extends AppCompatActivity {
 
         event = (Event) getIntent().getSerializableExtra("event");
 
-        TextView eventNameTV = (TextView) findViewById(R.id.eventName);
-        eventNameTV.setText(event.eventName);
+        TextView tvName = (TextView) findViewById(R.id.eventName);
+        tvName.setText(event.eventName);
+
+        TextView tvDesc = (TextView) findViewById(R.id.eventDescription);
+        tvDesc.setText(event.desc);
+
+        TextView tvLoc = (TextView) findViewById(R.id.eventLocation);
+        tvLoc.setText("Location\n"+event.loc);
+
+        TextView tvTime = (TextView) findViewById(R.id.eventTime);
+        tvTime.setText("Time\n"+event.time);
+
+        TextView tvPrizeMoney = (TextView) findViewById(R.id.eventPrizeMoney);
+        tvPrizeMoney.setText("Prize Money: Rs. " + event.prizeMoney);
+
+        TextView tvCoordName = (TextView) findViewById(R.id.eventCoordName);
+        tvCoordName.setText("Coordinator: "+event.coordName);
 
         Log.i("EVENTINFO", event.toString());
 
