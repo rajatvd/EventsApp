2a092be1a2052328a3591c2884bd7878fee3a440
rajatvd <rajatvd@gmail.com>
2017-05-23 12:51:10 +0530

    Changed init to get the testing event categories and events from a json file

--- app/src/main/java/shaastra/com/eventsapp/MainActivity.java
+++ app/src/main/java/shaastra/com/eventsapp/MainActivity.java
@@ -3,10 +3,18 @@ package shaastra.com.eventsapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -22,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<EventCategory> eventCategories = new ArrayList<EventCategory>();
         initEventCategories(eventCategories);
 
-
+        //TextView mainTitle = (TextView) findViewById(R.id.mainTitle);
+        //mainTitle.setText(loadJSONStringFromAsset("sampleEvents.json"));
 
 
         EventCategoryAdapter eventCategoryAdapter = new EventCategoryAdapter(this,eventCategories);
@@ -48,14 +57,54 @@ public class MainActivity extends AppCompatActivity {
         return i;
     }
 
-    public void initEventCategories(ArrayList<EventCategory> evcags){
-        EventCategory evcag1 = new EventCategory("Coding events",null);
+    public void initEventCategories(ArrayList<EventCategory> evCags){
+        /*EventCategory evcag1 = new EventCategory("Coding events",null);
         evcags.add(evcag1);
         EventCategory evcag2 = new EventCategory("Business events",null);
         evcags.add(evcag2);
         EventCategory evcag3 = new EventCategory("Quizzing events",null);
         evcags.add(evcag3);
         EventCategory evcag4 = new EventCategory("Robotics events",null);
-        evcags.add(evcag4);
+        evcags.add(evcag4);*/
+
+        String samplesString = loadJSONStringFromAsset("sampleEvents.json");
+        try {
+            JSONObject sampleFile = new JSONObject(samplesString);
+            JSONArray eventCags = sampleFile.getJSONArray("eventCategories");
+            for(int i=0;i<eventCags.length();i++){
+                JSONObject evCagJSON = eventCags.getJSONObject(i);
+
+                ArrayList<Event> evs = new ArrayList<Event>();
+                JSONArray evsJSON = evCagJSON.getJSONArray("events");
+                for(int j=0;j<evsJSON.length();j++){
+                    JSONObject ev = evsJSON.getJSONObject(j);
+                    Event event = new Event(ev);
+                    evs.add(event);
+                }
+
+                EventCategory evCag = new EventCategory(evCagJSON.getString("eventCategoryName"), evs);
+                evCags.add(evCag);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    public String loadJSONStringFromAsset(String filename) {
+        String json = null;
+        try {
+            InputStream is = this.getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
 }
